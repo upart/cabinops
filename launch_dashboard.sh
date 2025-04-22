87cd4f85d1dbbc13b3ef3d5bc5ca244564a1ae1f
ab <arnulf.barth@nextevolution.de>
2025-04-22 05:42:11 +0200

    remove dashboard script

--- launch_dashboard.sh
+++
@@ -1,3 +0,0 @@
-helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/ || true
-helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard || true
-kubectl -n kubernetes-dashboard port-forward svc/kubernetes-dashboard-kong-proxy 8443:443 || true
